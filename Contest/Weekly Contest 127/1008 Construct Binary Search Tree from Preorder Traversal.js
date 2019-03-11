18a57babe91c1754c4e7d73096bafa23371c095d
朱耀华-电子政务部-北京 <zhuyaohua01@gridsum.com>
2019-03-11 08:58:05 +0800

    finish 1008

--- Contest/Weekly Contest 127/1008 Construct Binary Search Tree from Preorder Traversal.js
+++ Contest/Weekly Contest 127/1008 Construct Binary Search Tree from Preorder Traversal.js
@@ -1,30 +1,27 @@
-// /**
-//  * Definition for a binary tree node.
-//  * function TreeNode(val) {
-//  *     this.val = val;
-//  *     this.left = this.right = null;
-//  * }
-//  */
-// /**
-//  * @param {number[]} preorder
-//  * @return {TreeNode}
-//  */
-// var bstFromPreorder = function (preorder) {
-//   let tree = new TreeNode(preorder[0])
-//   let treeArr = []
-//   for (let i = 1; i < preorder.length; i++) {
-//     let node = new TreeNode(preorder[i])
-//     treeArr.push(node)
-//   }
-//   for (let i = 1; i < preorder.length; i++) {
-//     let parent = null
-//     if (treeArr[i].val < treeArr[i - 1].val) {
-//       treeArr[i - 1].left = treeArr[i]
-//     } else if (treeArr[i].val > treeArr[i - 1].val) {
-//       treeArr[i - 1].right = treeArr[i]
-//     }
-//   }
-// };
-
-// considering use Recursion   search：根据前序遍历生成bst
-// 时间问题未能完成
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @return {TreeNode}
+ */
+var bstFromPreorder = function(preorder) {
+  if (preorder.length === 0) {
+    return null
+  }
+  let mid = preorder[0]
+  let tree = new TreeNode(mid)
+  var i = 0
+  for (; i < preorder.length; i++) {
+    if (preorder[i] > mid) {
+      break
+    }
+  }
+  tree.left = bstFromPreorder(preorder.slice(1, i))
+  tree.right = bstFromPreorder(preorder.slice(i))
+  return tree
+};
