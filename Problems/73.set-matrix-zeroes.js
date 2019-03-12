d003d66669ef1c59fb6c63b52c87e826e268d1c5
朱耀华-电子政务部-北京 <zhuyaohua01@gridsum.com>
2019-03-12 09:15:06 +0800

    solve 73

---
+++ Problems/73.set-matrix-zeroes.js
@@ -0,0 +1,100 @@
+/*
+ * @lc app=leetcode id=73 lang=javascript
+ *
+ * [73] Set Matrix Zeroes
+ *
+ * https://leetcode.com/problems/set-matrix-zeroes/description/
+ *
+ * algorithms
+ * Medium (39.03%)
+ * Total Accepted:    192.5K
+ * Total Submissions: 493.1K
+ * Testcase Example:  '[[1,1,1],[1,0,1],[1,1,1]]'
+ *
+ * Given a m x n matrix, if an element is 0, set its entire row and column to
+ * 0. Do it in-place.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 
+ * [
+ * [1,1,1],
+ * [1,0,1],
+ * [1,1,1]
+ * ]
+ * Output: 
+ * [
+ * [1,0,1],
+ * [0,0,0],
+ * [1,0,1]
+ * ]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 
+ * [
+ * [0,1,2,0],
+ * [3,4,5,2],
+ * [1,3,1,5]
+ * ]
+ * Output: 
+ * [
+ * [0,0,0,0],
+ * [0,4,5,0],
+ * [0,3,1,0]
+ * ]
+ * 
+ * 
+ * Follow up:
+ * 
+ * 
+ * A straight forward solution using O(mn) space is probably a bad idea.
+ * A simple improvement uses O(m + n) space, but still not the best
+ * solution.
+ * Could you devise a constant space solution?
+ * 
+ * [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+ */
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function (matrix) {
+  let col = false
+  const R = matrix.length
+  const C = matrix[0].length
+  for (let i = 0; i < R; i++) {
+    if (!col && matrix[i][0] === 0) {
+      col = true
+    }
+    for (let j = 1; j < C; j++) {
+      if (matrix[i][j] === 0) {
+        matrix[0][j] = 0
+        matrix[i][0] = 0
+      }
+    }
+  }
+
+  for (let i = 1; i < R; i++) {
+    for (let j = 1; j < C; j++) {
+      if (matrix[i][0] === 0 || matrix[0][j] === 0) {
+        matrix[i][j] = 0
+      }
+    }
+  }
+
+  if (matrix[0][0] === 0) {
+    for (let i = 1; i < C; i++) {
+      matrix[0][i] = 0
+    }
+  }
+
+  if (col) {
+    for (let j = 0; j < R; j++) {
+      matrix[j][0] = 0
+    }
+  }
+};
