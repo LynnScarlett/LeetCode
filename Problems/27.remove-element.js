0b9d03f233d7755fe42bb2875c67ce6e316da76d
zhuyaohua <buaa.zyh@gmail.com>
2019-03-13 20:08:44 +0800

    solve 27

--- Problems/27.remove-element.js
+++ Problems/27.remove-element.js
@@ -70,6 +70,13 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-    
+  let n = nums.length
+  for (let i = 0; i < n; i++){
+    if (nums[i] === val) {
+      nums.splice(i, 1)
+      i--
+    }
+  }
+  return nums.length
 };
 
